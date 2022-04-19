b8aefe8e95b55bb23d2950eb7bb3217470292fe7
jeongmin.lee <jaruda88@gmail.com>
2022-04-19 16:57:04 +0900

    add board paging

--- views/board_add.py
+++ views/board_add.py
@@ -1,9 +1,8 @@
 from flask import Blueprint, render_template, request
-import requests
-import json
 
 
 bp = Blueprint('board_add', __name__, url_prefix='/board_add')
+
 
 @bp.route('/')
 def board():
