64060b6a608a4aab007e865f4e95f9340700f860
jeongmin.lee <jaruda88@gmail.com>
2022-04-19 18:26:50 +0900

    board detail working

--- views/board.py
+++ views/board.py
@@ -6,3 +6,10 @@ bp = Blueprint('board', __name__, url_prefix='/board')
 @bp.route('/')
 def board():
     return render_template('board.html')
+
+
+@bp.route('/detail')
+def board_detail():
+    id = request.args.get('id')
+    print(id)
+    return render_template('board_detail.html')
